1c9bb767baf9119c0019721ab3eb73e5b497ff5d
xushuanglong <1872744675@qq.com>
2021-02-11 19:23:52 +0800

    build

---
+++ Leetcode/src/main/java/sql/Sql1.java
@@ -0,0 +1,13 @@
+package sql;
+
+/**
+ * @Description:
+ * @Author Goodenough
+ * @Date 2021/2/11 19:23
+ */
+public class Sql1 {
+
+    /*
+    select * from employees order by hire_date desc limit 1;
+     */
+}
