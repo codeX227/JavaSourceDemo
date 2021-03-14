844ba44e5076a2cc409d931c9f30fab9b1c1a54d
xushuanglong <1872744675@qq.com>
2021-03-14 16:13:07 +0800

    build

---
+++ Leetcode/src/main/java/solutions/Leetcode654.java
@@ -0,0 +1,46 @@
+package solutions;
+
+/**
+ * @Description 最大二叉树
+ * @Author Goodenough
+ * @Date 2021/3/14 16:11
+ */
+public class Leetcode654 {
+
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode build(int[] nums, int lo, int hi) {
+        // base case
+        if (lo > hi) {
+            return null;
+        }
+
+        // 找到数组中的最大值和对应的索引
+        int index = -1, maxVal = Integer.MIN_VALUE;
+        for (int i = lo; i <= hi; i++) {
+            if (maxVal < nums[i]) {
+                index = i;
+                maxVal = nums[i];
+            }
+        }
+
+        TreeNode root = new TreeNode(maxVal);
+        // 递归调用构造左右子树
+        root.left = build(nums, lo, index - 1);
+        root.right = build(nums, index + 1, hi);
+
+        return root;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
